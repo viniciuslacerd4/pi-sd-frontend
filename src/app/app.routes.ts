190497c9ee91ea05cd3b62dde54043f543578de1
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-08-31 14:12:54 -0300

    feat: add profile and not-found components
    components added for profile and 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -13,4 +15,7 @@ export const routes: Routes = [
       { path: 'register', component: RegisterComponent },
     ],
   },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' },
 ];
