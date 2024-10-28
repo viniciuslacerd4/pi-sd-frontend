f9c2df62e9558ae6da13e4e7dd80f8df09763845
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-10-28 17:18:35 -0300

    feat: add files for k8s deployment
    - update nginx configuration for backend proxy

--- src/app/services/http-app.service.ts
+++ src/app/services/http-app.service.ts
@@ -3,10 +3,7 @@ import { baseUrl } from '../utils/base-url';
 export abstract class HttpAppService {
   protected abstract endpoint: string;
 
-  protected getEndpoint(): string {
-    const fixedEndpoint = this.endpoint.startsWith('/')
-      ? this.endpoint.substring(1)
-      : this.endpoint;
-    return `${baseUrl}${fixedEndpoint}`;
+  protected getEndpoint(): string {    
+    return `${baseUrl}${this.endpoint}`;
   }
 }
