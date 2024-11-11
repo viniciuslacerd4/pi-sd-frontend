06e5bb876fad75ac76bcb80cbd10415c2636212d
Natanael Felix <f.natanael.felix.b@gmail.com>
2024-11-10 23:44:42 -0300

    refactor: update footer and landing components for authentication handling

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  isLoggedIn = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.jwtUser$.subscribe((user) => {
+      this.isLoggedIn = Boolean(user);
+    });
+  }
 }
